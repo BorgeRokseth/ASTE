17850953aec411dc05a28a108aadcb169acd813b
ansto4 <112398957+ansto4@users.noreply.github.com>
2023-03-20 11:31:15 +0100

    added collision index and grounding index

--- test_env.py
+++ test_env.py
@@ -1,16 +1,20 @@
-
-from os import environ
+import math
 from typing import NamedTuple
 import numpy as np
-
+import os
+import shapely.geometry as geo
 import seacharts
+import shapely.ops
 
-from ship_in_transit_simulator.models import EngineThrottleFromSpeedSetPoint, EnvironmentConfiguration, HeadingByRouteController, HeadingControllerGains,\
+from ship_in_transit_simulator.models import EnvironmentConfiguration, \
+    HeadingByRouteController, HeadingControllerGains, \
     LosParameters, ShipConfiguration, ShipModelSimplifiedPropulsion, \
     SimplifiedPropulsionMachinerySystemConfiguration, SimulationConfiguration, SpecificFuelConsumptionBaudouin6M26Dot3, \
-    SpecificFuelConsumptionWartila6L26, MachineryModeParams, MachineryMode, MachineryModes, ThrottleControllerGains, ThrottleFromSpeedSetPointSimplifiedPropulsion
+    SpecificFuelConsumptionWartila6L26, MachineryModeParams, MachineryMode, MachineryModes, \
+    ThrottleFromSpeedSetPointSimplifiedPropulsion
 
 from Ane_alterations.Boarder_solution import MapBoarderRegulator
+
 
 class TargetShip(NamedTuple):
     sim_time: float
@@ -21,12 +25,12 @@ class TargetShip(NamedTuple):
 
 
 class TargetShipMaker:
-    def __init__(self, 
-        ship_configuration: ShipConfiguration, 
-        environment: EnvironmentConfiguration, 
-        machinery_config: SimplifiedPropulsionMachinerySystemConfiguration, 
-        sea_lane: str
-    ) -> None:
+    def __init__(self,
+                 ship_configuration: ShipConfiguration,
+                 environment: EnvironmentConfiguration,
+                 machinery_config: SimplifiedPropulsionMachinerySystemConfiguration,
+                 sea_lane: str
+                 ) -> None:
         self.ship_config = ship_configuration
         self.env = environment
         self.machinery_config = machinery_config
@@ -34,12 +38,13 @@ class TargetShipMaker:
 
     def make_target_ship(self, start_time, initial_states: SimulationConfiguration) -> TargetShip:
         target_ship_model = ShipModelSimplifiedPropulsion(
-                ship_config=self.ship_config,
-                machinery_config=self.machinery_config,
-                environment_config=self.env,
-                simulation_config=initial_states
-            )
-        speed_controller = ThrottleFromSpeedSetPointSimplifiedPropulsion(kp=3, ki=0.02, time_step=initial_states.integration_step)
+            ship_config=self.ship_config,
+            machinery_config=self.machinery_config,
+            environment_config=self.env,
+            simulation_config=initial_states
+        )
+        speed_controller = ThrottleFromSpeedSetPointSimplifiedPropulsion(kp=3, ki=0.02,
+                                                                         time_step=initial_states.integration_step)
         heading_controller_gains = HeadingControllerGains(kp=4, kd=90, ki=0.01)
         los_guidance_parameters = LosParameters(
             radius_of_acceptance=600,
@@ -47,8 +52,11 @@ class TargetShipMaker:
             integral_gain=0.002,
             integrator_windup_limit=4000
         )
-        navigation_system = HeadingByRouteController(route_name=self.sea_lane, heading_controller_gains=heading_controller_gains, 
-            los_parameters=los_guidance_parameters, time_step=initial_states.integration_step, max_rudder_angle=self.machinery_config.max_rudder_angle_degrees * np.pi/180)
+        navigation_system = HeadingByRouteController(route_name=self.sea_lane,
+                                                     heading_controller_gains=heading_controller_gains,
+                                                     los_parameters=los_guidance_parameters,
+                                                     time_step=initial_states.integration_step,
+                                                     max_rudder_angle=self.machinery_config.max_rudder_angle_degrees * np.pi / 180)
         return TargetShip(
             sim_time=start_time + initial_states.simulation_time,
             start_time=start_time,
@@ -61,13 +69,12 @@ class TargetShipMaker:
 if __name__ == "__main__":
 
     size = 19000, 19000
-    center = 253536, 7045845 # east, north UTM-zone 33
+    center = 253536, 7045845  # east, north UTM-zone 33
     files = ['Trondelag.gdb']
-    enc = seacharts.ENC(files=files, border=True, center=center,size=size, new_data=False)
-
+    enc = seacharts.ENC(files=files, border=True, center=center, size=size, new_data=False)
 
     time_step = 0.5
-    #own_ship_route_name = "test_route.txt"      # Redundant, could be added directly into function on line 180
+    # own_ship_route_name = "test_route.txt"      # Redundant, could be added directly into function on line 180
     time_between_snapshots = 60
 
     main_engine_capacity = 2160e3
@@ -106,7 +113,7 @@ if __name__ == "__main__":
         shaft_generator_state=hybrid_shaft_gen_as_generator
     )
     pto_mode = MachineryMode(params=pto_mode_params)
-   
+
     mso_modes = MachineryModes([pto_mode, pto_mode])
 
     fuel_curves_me = SpecificFuelConsumptionWartila6L26()
@@ -147,14 +154,32 @@ if __name__ == "__main__":
     )
 
     ship_factory = TargetShipMaker(ship_configuration=ship_config,
-        environment=env_config,
-        machinery_config=machinery_config,
-        sea_lane="sea_lane_route.txt"
+                                   environment=env_config,
+                                   machinery_config=machinery_config,
+                                   sea_lane="sea_lane_route.txt"
+                                   )
+
+    initial_states_third_ship = SimulationConfiguration(
+        initial_north_position_m=7048985.01,
+        initial_east_position_m=246000.00,
+        initial_yaw_angle_rad=45 * np.pi / 180,
+        initial_forward_speed_m_per_s=5,
+        initial_sideways_speed_m_per_s=0,
+        initial_yaw_rate_rad_per_s=0,
+        integration_step=time_step,
+        simulation_time=10000,
     )
+
+    test_target_factory = TargetShipMaker(ship_configuration=ship_config,
+                                          environment=env_config,
+                                          machinery_config=machinery_config,
+                                          sea_lane="try_route.txt"
+                                          )
+
     first_target_ship = ship_factory.make_target_ship(start_time=0, initial_states=initial_states_first_ship)
     second_target_ship = ship_factory.make_target_ship(start_time=200, initial_states=initial_states_second_ship)
-    list_of_target_ships = [first_target_ship, second_target_ship]
-
+    third_target_ship = test_target_factory.make_target_ship(start_time=0, initial_states=initial_states_third_ship)
+    list_of_target_ships = [first_target_ship, second_target_ship, third_target_ship]
 
     test_own = MapBoarderRegulator(
         shipping_lane='own_ship_path_ane.txt',
@@ -164,11 +189,10 @@ if __name__ == "__main__":
     test_own.search_waypoints()
     test_own.update_txt_file()
 
-
     initial_states_own_ship = SimulationConfiguration(
         initial_north_position_m=test_own.start_north,
         initial_east_position_m=test_own.start_east,
-        initial_yaw_angle_rad=test_own.angle, # in rads, formula: degrees*np.pi/180
+        initial_yaw_angle_rad=test_own.angle,  # in rads, formula: degrees*np.pi/180
         initial_forward_speed_m_per_s=7,
         initial_sideways_speed_m_per_s=0,
         initial_yaw_rate_rad_per_s=0,
@@ -205,7 +229,7 @@ if __name__ == "__main__":
         heading_controller_gains=own_ship_heading_controller_gains,
         los_parameters=own_ship_los_guidance_parameters,
         time_step=initial_states_own_ship.integration_step,
-        max_rudder_angle=machinery_config.max_rudder_angle_degrees * np.pi/180
+        max_rudder_angle=machinery_config.max_rudder_angle_degrees * np.pi / 180
     )
 
     time_since_snapshot = time_between_snapshots
@@ -214,7 +238,83 @@ if __name__ == "__main__":
     ship_snap_shots = []
 
     # List for storing crashes
-    #crashes=[]
+    # crashes=[]
+
+    # List of lists for keeping distances between ships
+    dist_ships = []
+    for i in range(len(list_of_target_ships)):
+        dist_ships.append([])
+
+    def collision_index(list_of_lists,own_north, own_east, target_north, target_east):
+        ''' Requires an index to keep track of which list to append to, makes lists of distances between ships and
+            reconstructed them to an array after loop with np.column_stack.
+        '''
+
+        distance = math.sqrt((own_north - target_north) ** 2 + \
+                             (own_east - target_east) ** 2)
+        list_of_lists[index].append(distance)
+
+        # Collision index
+        if distance < 100:
+            collision_index =  distance / 100
+            print(f'Collision index:{collision_index}')
+
+
+    # List of lists for keeping track of distance to ground
+    dist_ground = []
+    folder = 'data\shapefiles'
+    for i in range(len(os.listdir(folder))):
+        dist_ground.append([])
+    print(os.listdir(folder))
+
+    def grounding_index(list_of_lists, own_north, own_east):
+        ''' Makes lists of distances to nearest ground, lists are reconstructed after loop with np.column_stack'''
+        geo_own = geo.Point(own_north, own_east)
+
+        for i in range(len(os.listdir(folder))):
+            if i == 0:
+                distance = geo_own.distance(enc.land.geometry)
+                list_of_lists[i].append(distance)
+
+                if distance < 50:
+                    print('ship grounded into land')
+
+            elif 1 <= i <= 9:
+                if i == 1:
+                    nr = 0
+                elif i == 2:
+                    nr = 5
+                elif i == 3:
+                    nr = 10
+                elif i == 4:
+                    nr = 20
+                if i == 5:
+                    nr = 50
+                elif i == 6:
+                    nr = 100
+                elif i == 7:
+                    nr = 200
+                elif i == 8:
+                    nr = 350
+                elif i == 9:
+                    nr = 500
+                distance = geo_own.distance(enc.seabed[nr].geometry)
+                list_of_lists[i].append(distance)
+
+                if distance < 50 and (i == 1 or i == 2 or i == 3 or i ==4):
+                    print(f'Ship grounded in sea bed at {nr} meters')
+
+            elif i == 10:
+                distance = geo_own.distance(enc.shore.geometry)
+                list_of_lists[i].append(distance)
+
+                if distance < 50:
+                    print('Ship grounded into shore')
+
+
+
+
+
 
 
     while own_ship.int.time <= own_ship.int.sim_time:
@@ -226,9 +326,14 @@ if __name__ == "__main__":
         own_ship_heading = own_ship.yaw_angle
         own_ship_speed = own_ship.forward_speed
 
+        # Check for grounding
+        if own_ship.int.time % 400 == 0:
+            grounding_index(dist_ground, own_ship_north_position, own_ship_east_position)
+
+
         # Check if own ship goes out of map and simulation should end
         if own_ship_north_position > test_own.north_side or own_ship_north_position < test_own.south_side \
-        or own_ship_east_position > test_own.east_side or own_ship_east_position < test_own.west_side:
+                or own_ship_east_position > test_own.east_side or own_ship_east_position < test_own.west_side:
             own_ship.int.time = own_ship.int.sim_time + 1
 
         # Find appropriate rudder angle and engine throttle
@@ -250,14 +355,15 @@ if __name__ == "__main__":
         # Add ownship snapshot to chart
         if time_since_snapshot > time_between_snapshots:
             snap_shot_id += 1
-            ship_snap_shots.append((snap_shot_id, int(own_ship_east_position), int(own_ship_north_position), int(own_ship.yaw_angle*180/np.pi), "green"))
+            ship_snap_shots.append((snap_shot_id, int(own_ship_east_position), int(own_ship_north_position),
+                                    int(own_ship.yaw_angle * 180 / np.pi), "green"))
             time_since_snapshot = 0
         time_since_snapshot += own_ship.int.dt
 
-
         # Check if we should launch target ship
+        index = 0
         for target_ship in list_of_target_ships:
-            if  global_time >= target_ship.start_time:
+            if global_time >= target_ship.start_time:
                 if global_time <= target_ship.sim_time:
                     # Find appropriate rudder angle and engine throttle (assume perfect measurements)
                     rudder_angle = target_ship.navigation_system.rudder_angle_from_route(
@@ -272,7 +378,7 @@ if __name__ == "__main__":
 
                     # Check if target ship goes outside map and should be terminated
                     if target_ship.ship_model.north > test_own.north_side or target_ship.ship_model.north < test_own.south_side \
-                    or target_ship.ship_model.east > test_own.east_side or target_ship.ship_model.east < test_own.west_side:
+                            or target_ship.ship_model.east > test_own.east_side or target_ship.ship_model.east < test_own.west_side:
                         list_of_target_ships.remove(target_ship)
 
                     # Update and integrate differential equations for current time step
@@ -280,29 +386,38 @@ if __name__ == "__main__":
                     target_ship.ship_model.update_differentials(engine_throttle=throttle, rudder_angle=rudder_angle)
                     target_ship.ship_model.integrate_differentials()
 
+            # Collision index
+            if own_ship.int.time % 60 == 0:
+                collision_index(dist_ships, own_ship.north, own_ship.east, target_ship.ship_model.north, target_ship.ship_model.east)
 
             # Add targetships snapshot to chart
             if time_since_snapshot > time_between_snapshots:
                 snap_shot_id += 1
-                ship_snap_shots.append((snap_shot_id, int(target_ship.ship_model.east), int(target_ship.ship_model.north),
-                                        int(target_ship.ship_model.yaw_angle * 180 / np.pi), "red"))
+                ship_snap_shots.append(
+                    (snap_shot_id, int(target_ship.ship_model.east), int(target_ship.ship_model.north),
+                     int(target_ship.ship_model.yaw_angle * 180 / np.pi), "red"))
                 time_since_snapshot = 0
             time_since_snapshot += own_ship.int.dt
 
+            index += 1
 
         own_ship.int.next_time()
 
+    # Reordering distance array collision index
+    dist_ships = np.column_stack((dist_ships))
 
+    # Reordering distance array grounding index
+    dist_ground = np.vstack(dist_ground)
 
+    #print(shapely.ops.nearest_points(own_ship.ship_drawings[0], enc.land))
+    print(dist_ships)
+    print(dist_ground)
 
     enc.add_vessels(*ship_snap_shots)
     enc.add_hazards(depth=5)
 
-    #enc.save_image("Test_own_ship_route_1")
+    #print(shapely.ops.nearest_points(own_ship.ship_drawings[0], enc.land))
+
+    #enc.save_image("Image to show basic collision index")
     #enc.fullscreen_mode(True)
-    enc.show_display()
-
-
-
-
-
+    #enc.show_display()
